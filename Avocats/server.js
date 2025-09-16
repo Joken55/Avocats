40ba7c57daa4a5238c56f1d52a1808fafdfb31fa
Joken55 <126733150+Joken55@users.noreply.github.com>
2025-09-16 02:29:49 +0200

    Update server.js

--- Avocats/server.js
+++ Avocats/server.js
@@ -44,6 +44,7 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       imgSrc: ["'self'", "data:", "https:"],
     },
   },
